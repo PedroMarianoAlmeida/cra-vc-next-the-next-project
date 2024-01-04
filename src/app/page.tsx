b0d917445a95f4d27cc2763e70b48014bf247fac
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-01-04 17:22:29 -0500

    remove not used button from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import { Button } from "@mui/material";
-
 export default function Home() {
-  return <main>
-    <Button variant="contained">Hello World</Button>
-  </main>;
+  return <main>Home</main>;
 }
