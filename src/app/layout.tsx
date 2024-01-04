a06a27d168849677cbbf6e297df40bfea816c914
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-01-03 21:17:34 -0500

    create backbone of project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import ThemeRegistry from "../../theme/ThemeRegistry";
+import PersistentDrawerLeft from "@/app/components/Drawer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ThemeRegistry>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <PersistentDrawerLeft>{children}</PersistentDrawerLeft>
+        </body>
       </ThemeRegistry>
     </html>
   );
---
+++ src/app/slow-api/page.tsx
@@ -0,0 +1,9 @@
+const SlowApiPage = () => {
+  return (
+    <main>
+      <h1>Slow API</h1>
+    </main>
+  );
+};
+
+export default SlowApiPage;
