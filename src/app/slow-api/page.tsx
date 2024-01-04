79ee35358773ed894a589fdb394e1269585784f3
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-01-03 21:29:50 -0500

    geting data from api

--- src/app/slow-api/page.tsx
+++ src/app/slow-api/page.tsx
@@ -1,7 +1,17 @@
-const SlowApiPage = () => {
+import ShowTime from "./ShowTime";
+
+export const dynamic = "force-dynamic";
+
+const SlowApiPage = async () => {
+  const response = await fetch(
+    "https://5rgdpun4qtidue3qwfhoc3vesm0ronor.lambda-url.us-east-2.on.aws/"
+  );
+  const data = await response.json();
+
   return (
     <main>
       <h1>Slow API</h1>
+      <ShowTime time={data.message} />
     </main>
   );
 };
